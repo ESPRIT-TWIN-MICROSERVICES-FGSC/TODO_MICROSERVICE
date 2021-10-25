c3a157f405f249cf2b600ee308d83c420e268ec3
ghadakhedri1 <ghada.khedri1@esprit.tn>
2021-10-25 12:39:42 +0100

    Eureka

--- helpers/eureka-helper.js
+++ helpers/eureka-helper.js
@@ -1,59 +1,54 @@
-const Eureka = require("eureka-js-client").Eureka;
+const request = require('request');
+const ip = require('ip');
 const config = require("config");
-const eurekaHost = config.get("EUREKA_CLIENT") || "127.0.0.1";
-const eurekaPort = 48658;
-const hostName = process.env.HOSTNAME || "localhost";
-const ipAddr = "172.0.0.1";
 
-exports.registerWithEureka = function (appName, PORT) {
-  const client = new Eureka({
-    instance: {
-      app: appName,
-      hostName: "192.168.99.100",
-      preferIpAddress: "true",
-      ipAddr: "192.168.99.100",
-      port: {
-        $: PORT,
-        "@enabled": "fasle",
-      },
-      vipAddress: appName,
-      dataCenterInfo: {
-        "@class": "com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo",
-        name: "MyOwn",
-      },
-    },
-    //retry 10 time for 3 minute 20 seconds.
-    eureka: {
-      host: "fgsc-eureka-server.herokuapp.com",
-      port: eurekaPort,
-      servicePath: "/eureka/apps",
-      maxRetries: 10,
-      requestRetryDelay: 2000,
-    },
-  });
+const hostname_ = 'https://fgsc-eureka-server.herokuapp.com/';
+const eurekaService = 'https://'+(config.get("EUREKA_CLIENT")  || hostname_)+'/eureka';
 
-  client.logger.level("debug");
-
-  client.start((error) => {
-    console.log(error || "todo service registered");
-  });
-
-  function exitHandler(options, exitCode) {
-    if (options.cleanup) {
-    }
-    if (exitCode || exitCode === 0) console.log(exitCode);
-    if (options.exit) {
-      client.stop();
-    }
-  }
-
-  client.on("deregistered", () => {
-    process.exit();
-  });
-
-  client.on("started", () => {
-    console.log("eureka host  " + eurekaHost);
-  });
-
-  process.on("SIGINT", exitHandler.bind(null, { exit: true }));
+module.exports = {
+   registerWithEureka: (appName, port) => {
+       console.log(`Registering ${appName} with Eureka`);
+       request.post({
+           headers: {'content-type': 'application/json'},
+           url: `${eurekaService}/${appName}`,
+           body: JSON.stringify({
+               instance: {
+                   hostName: `localhost`,
+                   instanceId: `${appName}-${port}`,
+                   vipAddress: `${appName}`,
+                   app: `${appName.toUpperCase()}`,
+                   ipAddr: ip.address(),
+                   status: `UP`,
+                   port: {
+                       $: port,
+                       "@enabled": true
+                   },
+                   dataCenterInfo: {
+                       "@class": `com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo`,
+                       name: `MyOwn`
+                   }
+               }
+           })
+       },
+       (error, response, body) => {
+           if(!error) {
+               console.log(`Registered with Eureka.`);
+               setInterval(() => {
+                   request.put({
+                       headers: {'content-type': 'application/json'},
+                       url: `${eurekaService}/${appName}/${appName}-${port}`
+                   }, (error, response, body => {
+                       if (error) {
+                           console.log('Sending heartbeat to Eureka failed.');
+                       } else {
+                           console.log('Successfully sent heartbeat to Eureka.');
+                       }
+                   }));
+               }, 50 * 1000);
+      
+           } else {
+               console.log(`Not registered with eureka due to: ${error}`);
+           }
+       });
+   }
 };
